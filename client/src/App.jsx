a605f1d845cd8ac395b13d83743e3640feee39c3
Shreyansh Tiwari <shreyansharipur@gmail.com>
2025-02-05 00:55:53 +0530

    added all the packages for frontend , tailwindCSS set up

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'; 
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-[20px] text-blue-600">Hey guys the development of our very own platform begins now.</h1>
+    </>
+  )
+}
+
+export default App
