ccc17435cf9f69603d79cac6ca679a7a5cfc4058
Shreyansh Tiwari <shreyansharipur@gmail.com>
2025-03-09 01:47:15 +0530

    Home Page completed-  feature and footer section added

---
+++ client/src/components/faq/FaqList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FaqItem from './faqItem'
+import {faqs} from '../../assets/data/faqs'
+
+const FaqList = () => {
+    return (
+        <ul className='mt-[30px]'>
+            {faqs.map((item,index)=> (
+                <FaqItem item={item} key={item} />
+            ))}
+        </ul>
+    )
+}
+
+export default FaqList
