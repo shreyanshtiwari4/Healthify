f107d79118b15929058813e5d92cfb46d0f822d3
shivdutt0411 <tiwari041102@gmail.com>
2025-04-15 00:45:31 +0530

    Price ticket added for doctors along with schedule

--- client/src/pages/Doctors/SidePanel.jsx
+++ client/src/pages/Doctors/SidePanel.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
-const SidePanel = () => {
+import convertTime from '../../utils/convertTime.js'
+const SidePanel = ({doctorId, ticketPrice, timeSlots}) => {
   return (
     <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
         <div className='flex items-center justify-between'>
             <p className='text__para mt-0 font-semibold'>Ticket Price</p>
             <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>
-                ₹ 500
+            {ticketPrice} $
             </span>
         </div>
 
@@ -16,30 +16,17 @@ const SidePanel = () => {
             </p>
 
             <ul className='mt-3'>
-                <li className='flex items-center justify-between mb-2'>
+            {timeSlots?.map((item,index)=>{
+                return (<li key = {index} className='flex items-center justify-between mb-2'>
                     <p className='text-[15px] leading-6 text-textColor font-semibold'>
-                        Sunday
+                       {item.day.charAt(0).toUpperCase()+item.day.slice(1)}
                     </p>
                     <p className='text-[15px] leading-6 text-textColor font-semibold'>
-                        4:00 PM - 6:00 PM
+                        {convertTime(item.startingTime)} - {convertTime(item.endingTime)}
                     </p>
-                </li>
-                <li className='flex items-center justify-between mb-2'>
-                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
-                        Tuesday
-                    </p>
-                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
-                        4:00 PM - 9:30 PM
-                    </p>
-                </li>
-                <li className='flex items-center justify-between mb-2'>
-                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
-                        WednesDay
-                    </p>
-                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
-                        4:00 PM - 9:30 PM
-                    </p>
-                </li>
+                </li>)
+
+            })}
             </ul>
         </div>
         <button className='btn px-2 w-full rounded-md'>Book Appointment</button>
