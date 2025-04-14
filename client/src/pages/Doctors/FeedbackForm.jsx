fdcc0db09eafd7fc264054aa338550f882dab531
shivdutt0411 <tiwari041102@gmail.com>
2025-04-15 00:44:46 +0530

    Giving feedback to doctor feature added

--- client/src/pages/Doctors/FeedbackForm.jsx
+++ client/src/pages/Doctors/FeedbackForm.jsx
@@ -1,15 +1,51 @@
 import React, {useState} from 'react'
 import { AiFillStar } from 'react-icons/ai'
+import { BASE_URL, token } from './../../config'
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import HashLoader from 'react-spinners/HashLoader'
 
 const FeedbackForm = () => {
 
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
   const [reviewText, setReviewText] = useState('');
+  const[loading , setLoading] = useState(false);
+  const {id} = useParams()
 
   const handleSubmitReview = async (e) => {
     e.preventDefault();
-    console.log(rating, reviewText);
+    setLoading(true);
+
+    try{
+        if(!rating || !reviewText){
+            setLoading(false);
+            toast.error('Rating and review fields are required');
+            return;
+        }
+
+        else{
+
+            const res = await fetch(`${BASE_URL}/doctors/${id}/reviews`,{
+                method : 'post',
+                headers:{
+                    'Content-type':'application/json',
+                    Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify({rating, reviewText})
+            })
+
+            const result = await res.json()
+            if(!res.ok){
+                throw new Error(result.message);
+            }
+            setLoading(false)
+            toast.success(result.message);
+        }
+    }catch(error){
+        setLoading(false)
+        toast.error(error.message)
+    }
   }
     
   return (
@@ -60,7 +96,7 @@ const FeedbackForm = () => {
             ></textarea>
         </div>
         <button type='submit' onClick={handleSubmitReview} className='btn'>
-            Submit Feedback
+            {loading? <HashLoader size={25} color='#fff'/>:'Submit Feedback'}
         </button>
     </form>
   )
